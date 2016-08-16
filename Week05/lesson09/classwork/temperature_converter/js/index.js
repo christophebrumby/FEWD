2bc55b0a5ed4f59b661101773f914471320330f3
christophebrumby <CB@MacBook-Pro-de-Christophe.local>
2016-08-16 21:01:46 +0100

    Edit to classwork lab

--- Week05/lesson09/classwork/temperature_converter/js/index.js
+++ Week05/lesson09/classwork/temperature_converter/js/index.js
@@ -4,30 +4,26 @@ function convert() {
   var f = document.querySelector("#f").value;
   var conversion, scale;
 
-  if (c !=="") { /* aka: the user filled out celsius */
+  if (c !=="") {
     conversion = c * 9/5 + 32;
     scale = "F";
   } else {
     conversion = (f - 32) * 5/9;
     scale = "C";
+    c =  conversion;
   }
 
   function switchBackground(thing) {
     document.querySelector("body").style.backgroundColor = thing;
   }
 
-// change the background color based on value of c
 if (c <= 0) {
-  // change background to blue
   switchBackground("blue");
 } else if (c > 0 && c <= 18 ) {
-  // change background to gray
   switchBackground("gray");
 } else if (c > 18 && c <= 30 ) {
-  // change background to orange
   switchBackground("orange");
 } else {
-  // change background to red
   switchBackground("red");
 }
 

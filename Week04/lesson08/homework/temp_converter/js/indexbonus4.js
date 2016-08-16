e4e70a492c9784ee7f9eba75473c11d4d287678e
christophebrumby <CB@MacBook-Pro-de-Christophe.local>
2016-08-16 15:59:41 +0100

    Includes middle thermometer that reacts to C and F inputs

--- Week04/lesson08/homework/temp_converter/js/indexbonus4.js
+++ Week04/lesson08/homework/temp_converter/js/indexbonus4.js
@@ -9,14 +9,64 @@ function conversion() {
       document.querySelector("#f").style.borderColor = "red";
   }
 
+  else if ((f === null || f === "") && (c < 0)){
+    f = c * 9 / 5 + 32;
+    document.querySelector("#f").value = Math.round(f*10)/10+"°F";
+    document.querySelector("#thermo").style.paddingTop = "160px";
+  }
+
+  else if ((f === null || f === "") && (c < 10)){
+    f = c * 9 / 5 + 32;
+    document.querySelector("#f").value = Math.round(f*10)/10+"°F";
+    document.querySelector("#thermo").style.paddingTop = "120px";
+  }
+
+  else if ((f === null || f === "") && (c < 20)){
+    f = c * 9 / 5 + 32;
+    document.querySelector("#f").value = Math.round(f*10)/10+"°F";
+    document.querySelector("#thermo").style.paddingTop = "80px";
+  }
+
+  else if ((f === null || f === "") && (c < 30)){
+    f = c * 9 / 5 + 32;
+    document.querySelector("#f").value = Math.round(f*10)/10+"°F";
+    document.querySelector("#thermo").style.paddingTop = "40px";
+  }
+
   else if (f === null || f === "") {
     f = c * 9 / 5 + 32;
     document.querySelector("#f").value = Math.round(f*10)/10+"°F";
+    document.querySelector("#thermo").style.paddingTop = "0px";
   }
 
-  else if (c === null || c === "") {
+  else if ((c === null || c === "") & (f < 32)) {
     c = (f - 32) * 5 / 9;
     document.querySelector("#c").value = Math.round(c*10)/10+"°C";
+    document.querySelector("#thermo").style.paddingTop = "160px";
+  }
+
+  else if ((c === null || c === "") & (f < 50)) {
+    c = (f - 32) * 5 / 9;
+    document.querySelector("#c").value = Math.round(c*10)/10+"°C";
+    document.querySelector("#thermo").style.paddingTop = "120px";
+  }
+
+  else if ((c === null || c === "") & (f < 68)) {
+    c = (f - 32) * 5 / 9;
+    document.querySelector("#c").value = Math.round(c*10)/10+"°C";
+    document.querySelector("#thermo").style.paddingTop = "80px";
+  }
+
+  else if ((c === null || c === "") & (f < 86)) {
+    c = (f - 32) * 5 / 9;
+    document.querySelector("#c").value = Math.round(c*10)/10+"°C";
+    document.querySelector("#thermo").style.paddingTop = "40px";
+  }
+
+    else if (c === null || c === "") {
+    c = (f - 32) * 5 / 9;
+    document.querySelector("#c").value = Math.round(c*10)/10+"°C";
+    document.querySelector("#thermo").style.paddingTop = "0px";
   }
 
   else {
@@ -38,6 +88,7 @@ function clear() {
   document.querySelector("#alert").innerHTML = "";
   document.querySelector("#c").style.borderColor = "grey";
   document.querySelector("#f").style.borderColor = "grey";
+  document.querySelector("#thermo").style.paddingTop = "200px";
 }
 
 document.querySelector("#reset").onclick = clear;

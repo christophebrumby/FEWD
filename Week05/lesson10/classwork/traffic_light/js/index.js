8297d3a504cc55f2b9e63fbeb34f55e9c0ab198b
christophebrumby <CB@MacBook-Pro-de-Christophe.local>
2016-08-18 20:15:41 +0100

    Traffic Light updated with jQuery workaround

--- Week05/lesson10/classwork/traffic_light/js/index.js
+++ Week05/lesson10/classwork/traffic_light/js/index.js
@@ -1,1 +1,33 @@
-//Implement the red light using jQuery. Don't forget to add the script tags.
+$('#stopButton').click(illuminateRed);
+$('#slowButton').click(illuminateYellow);
+$('#goButton').click(illuminateGreen);
+$('#clearButton').click(clearLights);
+
+function illuminateRed() {
+  clearLights();
+  $('#stopLight').css("backgroundColor", "red");
+}
+
+function illuminateYellow() {
+  clearLights();
+  $('#slowLight').css("backgroundColor", "yellow");
+}
+
+function illuminateGreen() {
+  clearLights();
+  $('#goLight').css("backgroundColor", "green");
+}
+
+function clearLights() {
+  $('#stopLight').css("backgroundColor", "black");
+  $('#slowLight').css("backgroundColor", "black");
+  $('#goLight').css("backgroundColor", "black");
+}
+
+/*function turnOff (buttonNode) {
+  buttonNode.style.backgroundColor = "black";
+}
+
+function fancyClearLights() {
+ document.querySelectorAll('.bulb').forEach(turnOff);
+}*/
